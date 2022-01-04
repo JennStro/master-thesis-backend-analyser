64b3381c36d78758ce5e8292f5a639f0f9f97455
Jenny Strømmen <jennys1994@gmail.com>
2022-01-04 09:29:37 +0100

    No equals method

--- src/main/java/visitor/BugFinderVisitor.java
+++ src/main/java/visitor/BugFinderVisitor.java
@@ -1,24 +1,43 @@
 package visitor;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import errors.BugReport;
+import errors.MissingEqualsMethodError;
 
-public class BugFinderVisitor  extends VoidVisitorAdapter<Void> {
+import java.util.List;
+
+public class BugFinderVisitor extends VoidVisitorAdapter<Void> {
 
     private BugReport report = new BugReport();
 
     @Override
-    public void visit(MethodDeclaration md, Void arg) {
-         super.visit(md, arg);
-         System.out.println("Method Name Printed: " + md.getName());
+    public void visit(MethodDeclaration declaration, Void arg) {
+         super.visit(declaration, arg);
+         System.out.println("Method Name Printed: " + declaration.getName());
+
     }
 
     @Override
-    public void visit(ClassOrInterfaceDeclaration decl, Void arg) {
-        super.visit(decl, arg);
-        System.out.println("Class:" + decl.getName());
+    public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
+        super.visit(declaration, arg);
+        report.setFileName(declaration.getNameAsString());
+        List<Node> children = declaration.getChildNodes();
+        System.out.println(children);
+        boolean classHasEqualsMethod = false;
+        for (Node child : children) {
+            if (child instanceof MethodDeclaration) {
+                MethodDeclaration equalsMethodCandidate = (MethodDeclaration) child;
+                if (equalsMethodCandidate.getNameAsString().equals("equals")) {
+                    classHasEqualsMethod = true;
+                }
+            }
+        }
+        if (!classHasEqualsMethod) {
+            report.addBug(new MissingEqualsMethodError(0, 0));
+        }
     }
 
     public BugReport getReport() {
