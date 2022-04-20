5c18a7ef14d9bbc235ec9ebf0e0d62b66e2fe50e
Jenny Strømmen <jennys1994@gmail.com>
2022-04-20 16:17:36 +0200

    bug: try no path

--- src/main/java/master/thesis/backend/analyser/Analyser.java
+++ src/main/java/master/thesis/backend/analyser/Analyser.java
@@ -16,8 +16,8 @@ public class Analyser {
 
     private AnalyserConfiguration configuration;
 
-    public final static String PATH_FOR_DEPENDENCIES = "src/main/resources";
-    public final static String FILE_PATH_FOR_DEPENDENCIES = PATH_FOR_DEPENDENCIES + "/";
+    public final static String PATH_FOR_DEPENDENCIES = "";
+    public final static String FILE_PATH_FOR_DEPENDENCIES = PATH_FOR_DEPENDENCIES + "";
     private final FileHandler dependencyHandler = new FileHandler();
 
     public BugReport analyse(String code) {
